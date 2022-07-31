965c0dab8db8c583dc09f7efcca9a2512da961ac
Muqsit <hackhack05@gmail.com>
2022-07-31 06:50:17 +0000

    Tick EditorTaskHandler only when tasks exist

--- src/cosmicpe/worldbuilder/editor/EditorManager.php
+++ src/cosmicpe/worldbuilder/editor/EditorManager.php
@@ -8,11 +8,14 @@ use cosmicpe\worldbuilder\editor\format\EditorFormatRegistry;
 use cosmicpe\worldbuilder\editor\task\copy\nbtcopier\NamedtagCopierManager;
 use cosmicpe\worldbuilder\editor\task\EditorTask;
 use cosmicpe\worldbuilder\Loader;
+use pocketmine\scheduler\TaskScheduler;
 
 final class EditorManager{
 
-	private EditorTaskHandler $task_handler;
+	private ?EditorTaskHandler $task_handler = null;
 	private EditorFormatRegistry $format_registry;
+	private TaskScheduler $scheduler;
+	private int $max_ops_per_tick;
 
 	public function __construct(){
 		NamedtagCopierManager::init();
@@ -20,8 +23,8 @@ final class EditorManager{
 	}
 
 	public function init(Loader $plugin) : void{
-		$this->task_handler = new EditorTaskHandler((int) $plugin->getConfig()->get("max-ops-per-tick"));
-		$plugin->getScheduler()->scheduleRepeatingTask($this->task_handler, 1);
+		$this->scheduler = $plugin->getScheduler();
+		$this->max_ops_per_tick = (int) $plugin->getConfig()->get("max-ops-per-tick");
 	}
 
 	public function getFormatRegistry() : EditorFormatRegistry{
@@ -29,6 +32,12 @@ final class EditorManager{
 	}
 
 	public function push(EditorTask $task) : void{
+		if($this->task_handler === null){
+			$this->task_handler = new EditorTaskHandler($this->max_ops_per_tick, function() : void{
+				$this->task_handler = null;
+			});
+			$this->scheduler->scheduleRepeatingTask($this->task_handler, 1);
+		}
 		$this->task_handler->handle($task);
 	}
 }
--- src/cosmicpe/worldbuilder/editor/EditorTaskHandler.php
+++ src/cosmicpe/worldbuilder/editor/EditorTaskHandler.php
@@ -4,16 +4,24 @@ declare(strict_types=1);
 
 namespace cosmicpe\worldbuilder\editor;
 
+use Closure;
 use cosmicpe\worldbuilder\editor\task\EditorTask;
+use pocketmine\scheduler\CancelTaskException;
 use pocketmine\scheduler\Task;
+use function count;
 
 final class EditorTaskHandler extends Task{
 
 	/** @var array<int, EditorTaskInfo> */
 	private array $tasks = [];
 
+	/**
+	 * @param int $max_operations_per_tick
+	 * @param Closure() : void $on_tasks_completion
+	 */
 	public function __construct(
-		private int $max_operations_per_tick
+		private int $max_operations_per_tick,
+		private Closure $on_tasks_completion
 	){}
 
 	public function handle(EditorTask $task) : void{
@@ -22,33 +30,36 @@ final class EditorTaskHandler extends Task{
 
 	public function onRun() : void{
 		$tasks_c = count($this->tasks);
-		if($tasks_c > 0){
-			$ops = max(1024, (int) floor($this->max_operations_per_tick / count($this->tasks)));
-			$completed = 0;
-			$ids = array_keys($this->tasks);
-			shuffle($ids);
-			foreach($ids as $id){
-				$limit = $ops;
-				$initial = $limit;
-				$info = $this->tasks[$id];
-				$generator = $info->getGenerator();
-				while(--$limit >= 0){
-					if(!$generator->send(true) || !$generator->valid()){
-						unset($this->tasks[$id]);
-						if(--$tasks_c > 0){
-							$ops += (int) floor($limit / count($this->tasks));
-						}
-						$info->getTask()->onCompletion();
-						break;
+		$ops = max(1024, (int) floor($this->max_operations_per_tick / $tasks_c));
+		$completed = 0;
+		$ids = array_keys($this->tasks);
+		shuffle($ids);
+		foreach($ids as $id){
+			$limit = $ops;
+			$initial = $limit;
+			$info = $this->tasks[$id];
+			$generator = $info->getGenerator();
+			while(--$limit >= 0){
+				if(!$generator->send(true) || !$generator->valid()){
+					unset($this->tasks[$id]);
+					if(--$tasks_c > 0){
+						$ops += (int) floor($limit / count($this->tasks));
 					}
-				}
-				if(isset($this->tasks[$id])){
-					$info->getTask()->onCompleteOperations($initial - $limit);
-				}
-				$completed += $initial - $limit;
-				if($completed >= $ops){
+					$info->getTask()->onCompletion();
+
+					if(count($this->tasks) === 0){
+						($this->on_tasks_completion)();
+						throw new CancelTaskException("No tasks left");
+					}
 					break;
 				}
+			}
+			if(isset($this->tasks[$id])){
+				$info->getTask()->onCompleteOperations($initial - $limit);
+			}
+			$completed += $initial - $limit;
+			if($completed >= $ops){
+				break;
 			}
 		}
 	}
