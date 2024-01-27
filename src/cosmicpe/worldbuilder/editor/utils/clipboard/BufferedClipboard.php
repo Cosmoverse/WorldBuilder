3982f6697223f6a643558b6ceac609b52643c44c
Muqsit <hackhack05@gmail.com>
2024-01-27 11:27:20 +0000

    Fix BufferedClipboard::getVolume() returning 0

--- src/cosmicpe/worldbuilder/editor/utils/clipboard/BufferedClipboard.php
+++ src/cosmicpe/worldbuilder/editor/utils/clipboard/BufferedClipboard.php
@@ -7,21 +7,18 @@ namespace cosmicpe\worldbuilder\editor\utils\clipboard;
 use Generator;
 use pocketmine\math\Vector3;
 use pocketmine\utils\Binary;
-use function feof;
 use function fread;
 use function fseek;
 use function fwrite;
 use function strlen;
 use function substr;
+use const SEEK_CUR;
 
 final class BufferedClipboard implements Clipboard{
 	use SimpleClipboardTrait{ __construct as __parentConstruct; }
 
 	/** @var resource */
 	private mixed $resource;
-
-	/** @var array<int, ClipboardEntry> */
-	private array $entries = [];
 
 	public function __construct(Vector3 $relative_position, Vector3 $minimum, Vector3 $maximum, mixed $resource){
 		$this->__parentConstruct($relative_position, $minimum, $maximum);
@@ -30,28 +27,21 @@ final class BufferedClipboard implements Clipboard{
 
 	public function get(int $x, int $y, int $z) : ?ClipboardEntry{
 		fseek($this->resource, 0);
-		while(!feof($this->resource)){
-			// 4 bytes - x
-			// 2 bytes - y
-			// 4 bytes - z
-			// 4 bytes - clipboard entry length
-			$data = fread($this->resource, 14);
-			$entry_length = Binary::readInt(substr($data, 10, 4));
-			$clipboard_data = fread($this->resource, $entry_length);
-
+		while(($data = fread($this->resource, 4)) !== false && $data !== ""){
+			$data = fread($this->resource, Binary::readInt($data));
 			$px = Binary::readInt(substr($data, 0, 4));
-			$py = Binary::readInt(substr($data, 4, 2));
+			$py = Binary::readShort(substr($data, 4, 2));
 			$pz = Binary::readInt(substr($data, 6, 4));
 			if($px === $x && $py === $y && $pz === $z){
-				return ClipboardEntry::fromRuntimeData($clipboard_data);
+				return ClipboardEntry::fromRuntimeData(substr($data, 10));
 			}
 		}
 		return null;
 	}
 
 	public function copy(int $x, int $y, int $z, ClipboardEntry $entry) : void{
-		$entry_data = $entry->toRuntimeData();
-		fwrite($this->resource, Binary::writeInt($x) . Binary::writeShort($y) . Binary::writeInt($z) . Binary::writeInt(strlen($entry_data)) . $entry_data);
+		$data = Binary::writeInt($x) . Binary::writeShort($y) . Binary::writeInt($z) . $entry->toRuntimeData();
+		fwrite($this->resource, Binary::writeInt(strlen($data)) . $data);
 	}
 
 	/**
@@ -62,22 +52,22 @@ final class BufferedClipboard implements Clipboard{
 	 */
 	public function getAll(&$x, &$y, &$z) : Generator{
 		fseek($this->resource, 0);
-		while(($data = fread($this->resource, 14)) !== false && $data !== ""){
-			// 4 bytes - x
-			// 2 bytes - y
-			// 4 bytes - z
-			// 4 bytes - clipboard entry length
+		while(($data = fread($this->resource, 4)) !== false && $data !== ""){
+			$data = fread($this->resource, Binary::readInt($data));
 			$x = Binary::readInt(substr($data, 0, 4));
 			$y = Binary::readShort(substr($data, 4, 2));
 			$z = Binary::readInt(substr($data, 6, 4));
-
-			$entry_length = Binary::readInt(substr($data, 10, 4));
-			$data = fread($this->resource, $entry_length);
-			yield ClipboardEntry::fromRuntimeData($data);
+			yield ClipboardEntry::fromRuntimeData(substr($data, 10));
 		}
 	}
 
 	public function getVolume() : int{
-		return count($this->entries);
+		$volume = 0;
+		fseek($this->resource, 0);
+		while(($data = fread($this->resource, 4)) !== false && $data !== ""){
+			fseek($this->resource, Binary::readInt($data), SEEK_CUR);
+			++$volume;
+		}
+		return $volume;
 	}
 }
